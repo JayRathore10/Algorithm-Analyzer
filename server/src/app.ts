c6e3ab5ba5b82aeea5747ee4fb47474485732770
Jay <jayrathore88155@gmail.com>
2026-04-24 20:16:41 +0530

    Connect frontend to backend and also call bubleSort algo

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,15 @@
 import express  , {Request , Response} from "express";
 import sortRouter from "./routes/sort.routes";
+import cors from "cors";``
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173" , 
+  methods: ["GET", "POST" , "DELETE" , "PUT"],
+  credentials: true
+}));
 
 app.get("/"  , (req : Request, res : Response)=>{
   res.send("Hi, Jexts here!")
